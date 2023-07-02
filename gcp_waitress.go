294759f50e76d67606aead93eb98bb6c9b8a1e00
joaquimborges <joaquim.borges1993@gmail.com>
2023-07-01 22:01:28 -0300

    feature: gcp waitress

--- gcp_waitress.go
+++ gcp_waitress.go
@@ -3,7 +3,6 @@ package toolkit
 import (
 	"cloud.google.com/go/storage"
 	"context"
-	"encoding/json"
 	"fmt"
 	"google.golang.org/api/option"
 	"google.golang.org/appengine"
@@ -12,9 +11,25 @@ import (
 	"net/http"
 )
 
+const (
+	defaultCacheControl = "Cache-Control:no-cache, max-age=0"
+)
+
 type GCPWaitressManager interface {
-	SaveFile(multipart.File, *multipart.FileHeader) (string, error)
-	ListFiles(bucket string) ([]string, error)
+	// SaveFile saves a file to the bucket and returns the name of the file or an error
+	// The prefix is used to create a folder in the bucket, if the prefix is empty, the file will be saved in the root of the bucket.
+	// Use the / after the prefix to create a folder, example: "images/"
+	// The cacheControl is used to set the cache control header in the file, if the cacheControl is empty, the default value will be used.
+	// The default value is: "Cache-Control:no-cache, max-age=0"
+	SaveFile(file multipart.File, fileHeader *multipart.FileHeader, prefix, cacheControl string) (string, error)
+
+	// ListFiles returns a list of files in the bucket
+	// The prefix is used to filter the files, if the prefix is empty, all files will be returned.
+	// Make sure to add the / after the prefix to filter the files, example: "images/"
+	ListFiles(prefix string) ([]string, error)
+
+	// DeleteFile use this method to delete a single file.
+	DeleteFile(filename string) error
 }
 
 type gcpWaitress struct {
@@ -23,14 +38,13 @@ type gcpWaitress struct {
 	ctx    context.Context
 }
 
-func NewGCPWaitress(bucket string, request *http.Request, gcpKey *GCPBucketAuthJson) (GCPWaitressManager, error) {
+// NewGCPWaitress creates a new GCP Waitress pointer to access the GCP Bucket
+// The bucket is the name of the bucket to access in Google Cloud Storage
+// The request is the http request to get the context, this is used to create the context for the GCP Client
+// The gcpKey is the json key to access the bucket, this key can be created in the Google Cloud Console and should have the Storage Admin role
+func NewGCPWaitress(bucket string, request *http.Request, gcpKey []byte) (GCPWaitressManager, error) {
 	ctx := appengine.NewContext(request)
-	bytes, jErr := json.Marshal(gcpKey)
-	if jErr != nil {
-		return nil, fmt.Errorf("error trying marchal gcp key :%s", jErr.Error())
-	}
-
-	client, err := storage.NewClient(ctx, option.WithCredentialsJSON(bytes))
+	client, err := storage.NewClient(ctx, option.WithCredentialsJSON(gcpKey))
 	if err != nil {
 		return nil, err
 	}
@@ -42,37 +56,59 @@ func NewGCPWaitress(bucket string, request *http.Request, gcpKey *GCPBucketAuthJ
 	}, nil
 }
 
-func (w *gcpWaitress) SaveFile(file multipart.File, fileHeader *multipart.FileHeader) (string, error) {
+func (w *gcpWaitress) SaveFile(file multipart.File, fileHeader *multipart.FileHeader, prefix, cacheControl string) (string, error) {
 	obj := w.bucket.
 		Object(fileHeader.Filename).
 		NewWriter(w.ctx)
 
-	obj.Attrs().CacheControl = "Cache-Control:no-cache, max-age=0"
+	if cacheControl == "" {
+		cacheControl = defaultCacheControl
+	}
+	obj.Attrs().CacheControl = cacheControl
+	obj.Attrs().Prefix = prefix
 
 	if _, err := io.Copy(obj, file); err != nil {
-		return "", fmt.Errorf("unable to write file to Google Storage: %s", err.Error())
+		return "", fmt.Errorf("unable to write file to Google Storage: %w", err)
 	}
 
 	if er := obj.Close(); er != nil {
-		return "", fmt.Errorf("unable to close the writer: %s", er.Error())
+		return "", fmt.Errorf("unable to close the writer: %w", er)
 	}
 	return obj.Attrs().Name, nil
 }
 
-func (w *gcpWaitress) ListFiles(bucket string) ([]string, error) {
+func (w *gcpWaitress) ListFiles(prefix string) ([]string, error) {
 	resp := make([]string, 0)
-	objects := w.client.Bucket(bucket).Objects(w.ctx, nil)
+	objects := w.bucket.Objects(w.ctx, &storage.Query{
+		Prefix:    prefix,
+		Delimiter: "/",
+	})
 
 	for {
 		attrs, err := objects.Next()
+		if err != nil {
+			return nil, fmt.Errorf("error loading file: %w", err)
+		}
 		if attrs == nil {
 			break
-		}
-
-		if err != nil {
-			return nil, fmt.Errorf("error loading file: %s", err.Error())
 		}
 		resp = append(resp, attrs.Name)
 	}
 	return resp, nil
+}
+
+func (w *gcpWaitress) DeleteFile(filename string) error {
+	oHandle := w.bucket.Object(filename)
+
+	attrs, err := oHandle.Attrs(w.ctx)
+	if err != nil {
+		return fmt.Errorf("error getting meta information about the object to delete it: %w", err)
+	}
+
+	if er := oHandle.
+		If(storage.Conditions{GenerationMatch: attrs.Generation}).
+		Delete(w.ctx); er != nil {
+		return fmt.Errorf("error trying delete obj: %w", er)
+	}
+	return nil
 }
